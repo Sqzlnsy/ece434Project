9d36915c9b22567240f2dafa6059fbfd2b1e750d
Harristhebest <81930315+Harristhebest@users.noreply.github.com>
2023-02-11 16:51:00 -0500

    Add files via upload

--- data_logger/readGPS.cpp
+++ data_logger/readGPS.cpp
@@ -90,7 +90,7 @@ void gyro_read(double accel_scale,double angle_scale){
 
 }
 void *write_gyro(void* ignore){
-        FILE *fo = fopen("gyroL.log", "w");
+        FILE *fo = fopen("gyroL.log", "a+");
     // Create the timer
         int i =0;
     // Set the gpsd daemon to streaming mode
@@ -107,7 +107,7 @@ void *write_gyro(void* ignore){
 
             double time = tv.tv_usec;
 
-            fprintf(fo, "%lf|%lf|%lf|%lf|%lf|%lf|%lf"
+            fprintf(fo, "%lf|%lf|%lf|%lf|%lf|%lf|%lf\n"
                         ,time, data_imu[0],data_imu[1],data_imu[2],data_imu[3],data_imu[4],data_imu[5]);
 
             fclose(fo);
@@ -115,12 +115,20 @@ void *write_gyro(void* ignore){
     return 0;
 }
 
+void* clear_file(void* ignore){
+    FILE *fo = fopen("gpsL.log", "w");
+    FILE *fg = fopen("gyroL.log", "w");
+    fprintf(fo, "");
+    fprintf(fg, "");
+    fclose(fo);
+    fclose(fg);
+}
 
 void* write_gps_file(void* ignor) {
     // Create the timer
     int i =0;
     // Set the gpsd daemon to streaming mode
-    FILE *fo = fopen("gpsL.log", "w");
+    FILE *fo = fopen("gpsL.log", "a+");
 
     int fde = fileno(fo);
         fcntl(fde, F_SETLKW, &lock);
@@ -174,7 +182,7 @@ void* write_gps_file(void* ignor) {
 
 
 
-            fprintf(fo, "%lf|%lf|%lf|%lf|%lf|%lf|%lf"
+            fprintf(fo, "%lf|%lf|%lf|%lf|%lf|%lf|%lf\n"
                         ,data_gps[0],data_gps[1] ,data_gps[2] ,data_gps[3] ,data_gps[4] ,data_gps[5],data_gps[6] ,data_gps[7]);
 
             fclose(fo);
@@ -202,7 +210,7 @@ int main(){
     }
     gps_stream(&gps_d, WATCH_ENABLE | WATCH_JSON, NULL);
     printf("start sleep...\n");
-    sleep(2);
+    sleep(1);
 
     FILE *scale_Accel = fopen("/sys/class/i2c-adapter/i2c-2/2-0068/iio:device1/in_accel_scale","r");
     FILE *scale_Angle = fopen("/sys/class/i2c-adapter/i2c-2/2-0068/iio:device1/in_anglvel_scale","r");
@@ -212,16 +220,35 @@ int main(){
     fscanf(scale_Angle, "%lf", &angle_scale);
 
     int count = 0;
+    clear_file(0);
+    struct timeval tv;
+    int start_time,now_time,delay_time;
+    int sleep_time=0;
+
     while(1){
+        gettimeofday(&tv, NULL);
+
+        start_time = tv.tv_usec;
+
         count += 1;
-        if (count == FREQUENCY/FREQUENCY_GYRO){
+        if (count % FREQUENCY/FREQUENCY_GYRO==0){
             write_gps_file(0);
-            count = 0;
         }
         gyro_read(accel_scale,angle_scale);
         write_gyro(0);
-        usleep((1000000)/FREQUENCY);
-        read_file(0);
+
+        gettimeofday(&tv, NULL);
+
+        now_time = tv.tv_usec;
+
+        delay_time = now_time - start_time;
+        sleep_time = (1000000)/FREQUENCY-delay_time;
+
+
+
+        usleep(sleep_time);
+
+        // read_file(0);
 
     }
 
