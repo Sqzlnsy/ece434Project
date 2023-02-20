89c68ed61b1718c401ea3bc7b6929141770c3cc7
Harristhebest <81930315+Harristhebest@users.noreply.github.com>
2023-02-20 16:20:35 -0500

    Update setup.sh

--- EKF/setup.sh
+++ EKF/setup.sh
@@ -1,4 +1,4 @@
 config-pin p9.21 uart
 config-pin p9.22 uart
-g++ -o fusionT cosd.cpp EKalmanGND.cpp insfilterNonholonomic.cpp readGPS.cpp quaternion.cpp sind.cpp vectorBuffer.c rt_nonfinite.cpp rtGetNaN.cpp rtGetInf.cpp test_rtwutil.cpp -lgps -pthread
+g++ -o fusionT dependencies/cosd.cpp dependencies/EKalmanGND.cpp dependencies/insfilterNonholonomic.cpp dependencies/quaternion.cpp dependencies/sind.cpp dependencies/vectorBuffer.c dependencies/rt_nonfinite.cpp dependencies/rtGetNaN.cpp dependencies/rtGetInf.cpp dependencies/test_rtwutil.cpp readGPS.cpp -lgps -pthread
 ./fusionT
