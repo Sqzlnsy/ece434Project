41c730fdbad599160c7d271fbae3e59603fc2f97
Harristhebest <wuq7@rose-hulman.edu>
2023-02-21 09:38:54 -0500

    makefile

--- main/setup.sh
+++
@@ -1,2 +0,0 @@
-g++ -o fusionT ../EKF/cosd.cpp ../EKF/EKalmanGND.cpp ../EKF/insfilterNonholonomic.cpp ../EKF/quaternion.cpp ../EKF/sind.cpp ../EKF/vectorBuffer.c ../EKF/rt_nonfinite.cpp ../EKF/rtGetNaN.cpp ../EKF/rtGetInf.cpp ../EKF/test_rtwutil.cpp readGPS.cpp -lgps -pthread
-./fusionT
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
+service gpsd restart
 config-pin P9.21 uart
 config-pin P9.22 uart
-service gpsd restart
 config-pin P9.19 i2c
 config-pin P9.20 i2c
 ip link set can1 type can bitrate 500000
